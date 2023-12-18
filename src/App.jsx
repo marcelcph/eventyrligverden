f1ab48b2bbda36f89fe51485db3f9e1a86601459
MemisGoz <55129239+MemisGoz@users.noreply.github.com>
2023-12-18 15:12:08 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const WORDPRESS_URL = import.meta.env.VITE_REACT_APP_WORDPRESS_URL;
+  console.log(import.meta.env);
+  console.log(WORDPRESS_URL);
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <button className="btn btn-secondary">Primary</button>
       <div>
-        <p>WordPress URL: {process.env.REACT_APP_WORDPRESS_URL}</p>
-        <p>Consumer Key: {process.env.REACT_APP_CONSUMER_KEY}</p>
-        <p>Consumer Secret: {process.env.REACT_APP_CONSUMER_SECRET}</p>
+        <p>Consumer Key: {WORDPRESS_URL}</p>
       </div>
     </>
   );
--- src/settings/Url.jsx
+++ src/settings/Url.jsx
@@ -1,13 +1,1 @@
-import React from "react";
 
-function Url() {
-  return (
-    <div>
-      <p>WordPress URL: {env.wordpress_url}</p>
-      <p>Consumer Key: {env.consumer_key}</p>
-      <p>Consumer Secret: {env.consumer_secret}</p>
-    </div>
-  );
-}
-
-export default Url;
