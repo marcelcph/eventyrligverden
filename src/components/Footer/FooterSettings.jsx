c0e63c07937b596720a5ab313dc089b017fedade
MemisGoz <55129239+MemisGoz@users.noreply.github.com>
2023-12-20 19:48:54 +0100

    f

---
+++ src/components/Footer/FooterSettings.jsx
@@ -0,0 +1,22 @@
+import placeholderImg from "../../assets/images/placeholder.png";
+
+const FooterSettings = [
+    {
+      logo: placeholderImg,
+      beskrivelse: "Kort beskrivelse af fx virksomhed",
+
+      overskrift1: "overskrift 1",
+      overskrift2: "overskrift 2",
+      overskrift3: "overskrift 3",
+      overskrift4: "overskrift 4",
+
+      telefonNr: "+45 11 11 11 11",
+      email: "test@test.dk",
+      adresse: "adresse her",
+      åbningstider: "skriv åbnings tider",
+      cvr: "cvr her",
+
+      copyRight:"@firmanavn - Dato ",
+    },
+  ];
+  export default FooterSettings;
