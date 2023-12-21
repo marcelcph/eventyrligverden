2a1cf84abe27853dac9823248de3965cff18f659
MemisGoz <55129239+MemisGoz@users.noreply.github.com>
2023-12-21 23:55:33 +0100

    CAROUSEL

--- src/components/Produktlogo/ProduktCarousel.jsx
+++ src/components/Produktlogo/ProduktCarousel.jsx
@@ -1,24 +1,257 @@
-import ProduktCarouselSettings from "./ProduktCarouselSettings.jsx";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import { Url } from "../../utils/Url";
 
 function ProduktCarousel() {
-  const { logo1, logo2, logo3, logo4 } = ProduktCarouselSettings;
+  const [featuredProducts, setFeaturedProducts] = useState([]);
 
+  useEffect(() => {
+    const fetchFeaturedProducts = async () => {
+      try {
+        const response = await axios.get(Url.WORDPRESS_WOO_URL, {
+          params: {
+            featured: true,
+          },
+        });
+
+        setFeaturedProducts(response.data);
+      } catch (error) {
+        console.error("Error fetching featured products:", error);
+      }
+    };
+
+    fetchFeaturedProducts();
+  }, []);
   return (
     <>
-      <h1 className="text-center text-6xl ">Produkter</h1>
-      <div className="carousel rounded-box">
-        <div className="carousel-item max-w-sm">
-          <img src={logo1} alt="Placeholder" />
+      
+      <div className="2xl:mx-auto 2xl:container flex justify-center">
+        <div className="2xl:px-20 px-6 py-12 w-full lg:w-4/5">
+          {/* Carousel for Small-Sized Screen */}
+          <CarouselProvider
+            className="relative block sm:hidden"
+            naturalSlideWidth={100}
+            isIntrinsicHeight={true}
+            totalSlides={3}
+            visibleSlides={1}
+            step={1}
+            infinite={true}
+          >
+            <div className="js-flickity flex justify-center items-center">
+              <ButtonBack
+                role="button"
+                aria-label="slide backward"
+                className="w-12 h-12 md:w-14 md:h-14 rounded-full flex justify-center items-center bg-white border border-gray-300 hover:bg-gray-400 absolute z-30 left-0 ml-8 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer"
+                id="prev"
+              >
+                <svg
+                  width={8}
+                  height={14}
+                  viewBox="0 0 8 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7 1L1 7L7 13"
+                    stroke="black"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </ButtonBack>
+              <Slider>
+                {featuredProducts.map((product) => (
+                  <Slide key={product.id} index={product.id}>
+                    <div className="gallery-cell w-full h-full">
+                      <div className="relative w-full h-full lg:block hidden">
+                        <img
+                          src={product.images[0].src}
+                          alt={product.name}
+                          className="object-center object-cover w-full h-full"
+                        />
+                        <div className="pl-6 pb-6 lg:pl-8 lg:pb-8 absolute left-0 bottom-0">
+                          <h1 className="text-xl leading-5 lg:text-2xl lg:leading-normal font-medium text-white">
+                            {product.name}
+                          </h1>
+                        </div>
+                      </div>
+                      <div className="relative w-full h-full lg:hidden">
+                        <img
+                          src={product.images[0].src}
+                          alt={product.name}
+                          className="object-center object-cover w-full h-full"
+                        />
+                        <div className="pl-6 pb-6 lg:pl-8 lg:pb-8 absolute left-0 bottom-0">
+                          <h1 className="text-xl leading-5 lg:text-2xl lg:leading-normal font-medium text-white">
+                            {product.name}
+                          </h1>
+                        </div>
+                      </div>
+                    </div>
+                  </Slide>
+                ))}
+              </Slider>
+              <ButtonNext
+                role="button"
+                aria-label="slide forward"
+                className="w-12 h-12 md:w-14 md:h-14 rounded-full flex justify-center items-center bg-white border border-gray-300 hover:bg-gray-400 absolute z-30 right-0 mr-8 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800"
+                id="next"
+              >
+                <svg
+                  width={8}
+                  height={14}
+                  viewBox="0 0 8 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1 1L7 7L1 13"
+                    stroke="black"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </ButtonNext>
+            </div>
+          </CarouselProvider>
+
+          {/* Carousel for Medium and Large-Sized Screen */}
+          <CarouselProvider
+            className="relative hidden sm:block"
+            naturalSlideWidth={100}
+            isIntrinsicHeight={true}
+            totalSlides={2.5}
+            visibleSlides={1}
+            step={1}
+            infinite={true}
+            currentSlide={1}
+          >
+            <div className="js-flickity flex justify-center items-center">
+              <ButtonBack
+                role="button"
+                aria-label="slide backward"
+                className="w-12 h-12 md:w-14 md:h-14 rounded-full flex justify-center items-center bg-white border border-gray-300 hover:bg-gray-400 absolute z-30 left-0 ml-8 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer"
+                id="prev"
+              >
+                <svg
+                  width={8}
+                  height={14}
+                  viewBox="0 0 8 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7 1L1 7L7 13"
+                    stroke="black"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </ButtonBack>
+              <Slider>
+                {featuredProducts.map((product) => (
+                  <Slide key={product.id} index={product.id}>
+                    <div className="gallery-cell w-full h-full">
+                      <div className="relative w-full h-full lg:block hidden">
+                        <img
+                          src={product.images[0].src}
+                          alt={product.name}
+                          className="object-center object-fit w-full h-full"
+                        />
+                        <div className="pl-6 pb-6 lg:pl-8 lg:pb-8 absolute left-0 bottom-0">
+                          <h1 className="text-xl leading-5 lg:text-2xl lg:leading-normal font-medium text-white">
+                            {product.name}
+                          </h1>
+                        </div>
+                      </div>
+                      <div className="relative w-full h-full lg:hidden">
+                        <img
+                          src={product.images[0].src}
+                          alt={product.name}
+                          className="object-center object-cover w-full h-full"
+                        />
+                        <div className="pl-6 pb-6 lg:pl-8 lg:pb-8 absolute left-0 bottom-0">
+                          <h1 className="text-xl leading-5 lg:text-2xl lg:leading-normal font-medium text-white">
+                            {product.name}
+                          </h1>
+                        </div>
+                      </div>
+                    </div>
+                  </Slide>
+                ))}
+              </Slider>
+              <ButtonNext
+                role="button"
+                aria-label="slide forward"
+                className="w-12 h-12 md:w-14 md:h-14 rounded-full flex justify-center items-center bg-white border border-gray-300 hover:bg-gray-400 absolute z-30 right-0 mr-8 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800"
+                id="next"
+              >
+                <svg
+                  width={8}
+                  height={14}
+                  viewBox="0 0 8 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1 1L7 7L1 13"
+                    stroke="black"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </ButtonNext>
+            </div>
+          </CarouselProvider>
         </div>
-        <div className="carousel-item max-w-sm">
-          <img src={logo2} alt="Placeholder" />
-        </div>
-        <div className="carousel-item max-w-sm">
-          <img src={logo3} alt="Placeholder" />
-        </div>
-        <div className="carousel-item max-w-sm">
-          <img src={logo4} alt="Placeholder" />
-        </div>
+
+        <style>
+          {`
+                    .gallery-cell {
+                        height: 486px;
+                        padding-right:15px;
+                    }
+                    @media (min-width: 300px) and (max-width: 420px) {
+                        .gallery-cell {
+                            height: 286px !important;
+                            
+                            
+                        }
+                    }
+                    
+                    @media (max-width: 640px) {
+                        .gallery-cell {
+                            padding-right:0;
+                        }
+                    }
+
+                    .carousel__sliderLarge {
+                        padding-left: 20%;
+                        padding-right: 20%;
+                    }
+
+                    /* gives us the illusion of spaces between the slides */
+                    .carousel__inner-slideLarge {
+                        width: calc(100% - 20px);
+                        height: calc(100% - 20px);
+                        left: 10px;
+                        top: 10px;
+                        
+                    }
+                `}
+        </style>
       </div>
     </>
   );
